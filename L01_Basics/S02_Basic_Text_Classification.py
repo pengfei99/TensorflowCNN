cf3941d6e4f6ed210cf405707c9922800b397c8b
pengfei <liu.pengfei@hotmail.fr>
2021-07-06 13:56:41 +0200

    update

--- L01_Basics/S02_Basic_Text_Classification.py
+++ L01_Basics/S02_Basic_Text_Classification.py
@@ -1,5 +1,7 @@
 import os
+import re
 import shutil
+import string
 
 import tensorflow as tf
 
@@ -22,6 +24,13 @@ def step1() -> str:
     home_path = tf.keras.utils.get_file("aclImdb_v1", _URL,
                                         untar=True)
     return os.path.join(os.path.dirname(home_path), 'aclImdb')
+
+
+def custom_standardization(input_data):
+    lower_case = tf.strings.lower(input_data)
+    remove_html_tag = tf.strings.regex_replace(lower_case, '<br />', ' ')
+    return tf.strings.regex_replace(remove_html_tag,'[%s]' % re.escape(string.punctuation))
+
 
 
 def main():
@@ -53,12 +62,33 @@ def main():
     # into three splits: train, validation and test. We already have train, test. So let's
     # creat a validation data set using an 80:20 split of the training data by using
     # validation_split argument.
+    # And the subset argument defines this function returns the training (0.8) split
+    # To avoid overlap between the split. We need to use a fix random seed, or set
+    # shuffle=False. In our case, we choose to set seed=42.
     raw_imdb_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
         train_dir, batch_size=32, validation_split=0.2, subset='training', seed=42)
-    for text_batch,label_batch in raw_imdb_train_ds.take(2):
+
+    # Prepare the validation data, note it has the same seed value
+    raw_imdb_validation_ds = tf.keras.preprocessing.text_dataset_from_directory(
+        train_dir, batch_size=32, validation_split=0.2, subset='validation', seed=42)
+    # Prepare the test data
+    raw_imdb_test_ds = tf.keras.preprocessing.text_dataset_from_directory(test_dir, batch_size=32)
+
+    # show the first 3 comments and labels for 2 batch. You can notice the batch size is
+    # 32 as we defined before
+    for text_batch, label_batch in raw_imdb_train_ds.take(2):
+        print("label_batch size: {}".format(len(label_batch)))
         for i in range(3):
             print("Review: {}".format(text_batch.numpy()[i]))
             print("Label: {}".format(label_batch.numpy()[i]))
+
+    # Get classification string name
+    classification_str_names = raw_imdb_train_ds.class_names
+    print("class name in str: {}".format(classification_str_names))
+
+    # Step4: Data cleaning
+    # We have noticed in the reviews, there are various HTML tags in it (e.g. <br />). So
+    # we need to remove them
 
 
 if __name__ == "__main__":
